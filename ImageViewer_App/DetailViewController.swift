6d6c3b89a4a8643780b55368fa37562aa73c70f0
Testing_author <testing_beta@mail.com>
2020-02-02 16:31:28 +0200

    In the DetailViewController class was added a new function which allows users to share the pictures in their library, as well as making it their contact picture, and sharing it to FB or TW. Also edited the info.plist in order to include the message required for when saving "pictures" on the user's phone (i.e. in their library). A new property was added  "Privacy - photo library addition".

--- ImageViewer_App/DetailViewController.swift
+++ ImageViewer_App/DetailViewController.swift
@@ -18,6 +18,8 @@ class DetailViewController: UIViewController {
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
             
@@ -35,4 +37,16 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = true
     }
 
+    @objc func shareTapped() {
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+            else { print ("No image was found")
+                return
+        }
+        
+        // passing the above "image" in the activity items array
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }
